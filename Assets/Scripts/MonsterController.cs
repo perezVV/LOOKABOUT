a30be155a8ce55e89098e8b1258ec7406ed50df7
Austin Nguyen <a_nguyen202@u.pacific.edu>
2023-10-20 14:27:13 -0700

    add detection for if the monster has been chasing player for certain amount of time

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -2,19 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MonsterController : MonoBehaviour
 {
     [SerializeField] private int detectionRange;
     [SerializeField] private float velocity;
+    [SerializeField] private int maxStamina;
 
     private Transform target;
 
+    private bool drainingStamina;
+    
+    private int currentStamina;
+    
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentStamina = maxStamina;
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         // TODO make sure Monster does not spawn inside of an object/wall
@@ -28,20 +35,39 @@ public class MonsterController : MonoBehaviour
             // move towards player if within detection range
             Vector3 move = (target.position - transform.position).normalized;
             rb.velocity = move * velocity;
+            StartCoroutine("DrainStamina");
         }
         else
         {
             rb.velocity = Vector2.zero;
+            currentStamina = maxStamina;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log($"{other}");
+        // Debug.Log($"monster collided with {other}");
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Game Over :(");
             // TODO implement game over screen from here
+        }
+    }
+    
+    private IEnumerator DrainStamina()
+    {
+        if (!drainingStamina)
+        {
+            drainingStamina = true;
+            yield return new WaitForSeconds(1);
+            currentStamina = Mathf.Clamp(currentStamina - 1, 0, maxStamina);
+            if (currentStamina == 0)
+            {
+                Debug.Log("Monster is tired :/");
+                currentStamina = maxStamina;
+                //TODO respawn monster at proper location
+            }
+            drainingStamina = false;
         }
     }
 }
